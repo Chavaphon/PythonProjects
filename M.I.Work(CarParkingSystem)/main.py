a764d8cb611c727bffafae30ba4f651c62860086
Chavaphon "Bard" Intarasuriyawong <62789383+Chavaphon@users.noreply.github.com>
2023-01-27 16:42:19 +0700

    Update main.py

--- M.I.Work(CarParkingSystem)/main.py
+++ M.I.Work(CarParkingSystem)/main.py
@@ -3,33 +3,83 @@ DaysPassed = 1
 SpotUsed = []
 frequency = {}
 dailyTotal = 0
+sameDay = False
 MaxStayHour = {
     "sunday"    : 8,
+    "Sunday"    : 8,
+    "sun"    : 8,
+    "Sun"    : 8,
     "monday"    : 2,
-    "tuesday"   : 2,
-    "wednesday" : 2,
-    "thursday"  : 2,
+    "Monday"    : 2,
+    "mon"    : 2,
+    "Mon"    : 2,
+    "tuesday"    : 2,
+    "Tuesday"    : 2,
+    "tue"    : 2,
+    "Tue"    : 2,
+    "wednesday"    : 2,
+    "Wednesday"    : 2,
+    "wed"    : 2,
+    "Wed"    : 2,
+    "thursday"    : 2,
+    "thursday"    : 2,
+    "thu"    : 2,
+    "Thu"    : 2,
     "friday"    : 2,
-    "saturday"  : 4
+    "Friday"    : 2,
+    "fri"    : 2,
+    "Fri"    : 2,
+    "saturday"  : 4,
+    "Saturday"  : 4,
+    "sat"  : 4,
+    "Sat"  : 4
 }
 PricePerHour = {
     "sunday"    : 2,
+    "Sunday"    : 2,
+    "sun"    : 2,
+    "Sun"    : 2,
     "monday"    : 10,
-    "tuesday"   : 10,
-    "wednesday" : 10,
-    "thursday"  : 10,
+    "Monday"    : 10,
+    "mon"    : 10,
+    "Mon"    : 10,
+    "tuesday"    : 10,
+    "Tuesday"    : 10,
+    "tue"    : 10,
+    "Tue"    : 10,
+    "wednesday"    : 10,
+    "Wednesday"    : 10,
+    "wed"    : 10,
+    "Wed"    : 10,
+    "thursday"    : 10,
+    "thursday"    : 10,
+    "thu"    : 10,
+    "Thu"    : 10,
     "friday"    : 10,
-    "saturday"  : 3
+    "Friday"    : 10,
+    "fri"    : 10,
+    "Fri"    : 10,
+    "saturday"  : 3,
+    "Saturday"  : 3,
+    "sat"  : 3,
+    "Sat"  : 3
 }
 try:
     while 1:
 
 #New day
         print(f"day {DaysPassed}")
-        day = input(f"Day: ")
+
+        if sameDay == True:
+            day = today
+            print(f"Today is {day}")
+        else:
+            day = input(f"Day: ")
+            today = day
+
         if day in MaxStayHour:
             try:
-                time = float(input(f"Time: "))
+                time = float(input(f"Time of arrival: "))
                 while time > 24 or time < 8:
                     time = float(input(f"That time is closed: "))
                     continue
@@ -76,6 +126,10 @@ try:
                         discount = 1
                     price = duration * PricePerHour[day] * discount
                     print(f"Price: {price}\n")
+                else:
+                    print(f"You can't stay THAT long\n")
+                    print("\n")
+                    continue
 
 #Evening
             elif time > 16:
@@ -90,6 +144,10 @@ try:
                         discount = 1
                     price = duration * 2 * discount
                     print(f"Price: {price}\n")
+                else:
+                    print(f"You can't stay THAT long\n")
+                    print("\n")
+                    continue
 
 #Amount to pay
             amountPayed = float(input("How much will you pay: "))
@@ -97,15 +155,14 @@ try:
                 amountPayed = int(input("That amount is less than the price, enter again: "))
             endDay = str(input("Payed. Next day?: "))
             if endDay == "" or endDay == "yes":
+                sameDay = False
                 print("\n")
                 dailyTotal += int(amountPayed)
                 print(f"Amount of money we got today: {dailyTotal}$")
                 dailyTotal = 0
                 DaysPassed += 1
-            elif duration > MaxStayHour[day]:
-                print(f"You can't stay THAT long\n")
-                print("\n")
-                continue
+            else:
+                sameDay = True
         else:
             print(f"What day is {day}?!\n")
             print("\n")
