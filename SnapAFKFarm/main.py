bce9015bf43b98b2e523c15f79baa1479612c3bb
Chavaphon "Bard" Intarasuriyawong <62789383+Chavaphon@users.noreply.github.com>
2023-05-16 19:08:26 +0700

    Update main.py

--- SnapAFKFarm/main.py
+++ SnapAFKFarm/main.py
@@ -1,9 +1,6 @@
 import pyautogui
 from time import sleep
 import keyboard
-
-MatchCounter = 0
-WinCounter = 0
 try:
     print(f"Initiating...")
     sleep(3)
@@ -15,12 +12,8 @@ try:
             sleep(1)
         else:
             print(f"Playing...")
-            if pyautogui.locateOnScreen("Victory.PNG", confidence=0.8):
-                WinCounter += 1
             pyautogui.click(1281, 1002)
             sleep(1)
-    print(f"Played {MatchCounter} matches\n Win rate: {(WinCounter / MatchCounter) * 100}%")
     print(f"Terminated!")
 except KeyboardInterrupt:
-    print(f"Played {MatchCounter} matches\n Win rate: {(WinCounter / MatchCounter) * 100}%")
     print(f"Terminated!")
